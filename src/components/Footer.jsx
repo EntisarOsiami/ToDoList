a3c417bfe714902f88982eb97c0f22ac392666e7
Entisar <Code.g.light@gmail.com>
2025-05-28 13:49:12 +0300

    footer color change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
   return (
     <footer>
-      <div className='text-center text-green-500 text-base font-bold py-4'>
+      <div className='text-center text-blue-500 text-base font-bold py-4'>
         <p> &copy; 2025 ToDo List App. All rights reserved.</p>
         
           </div>
